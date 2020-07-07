b88c5af1d15c00ad4581cffb6cd84b8988756cbd
cihat <cihat02020202@gmail.com>
2020-07-07 14:05:23 +0300

    added underscore.js this is javaScript library

--- arraysinJS/underscore.js
+++ arraysinJS/underscore.js
@@ -1,1 +1,10 @@
 var und = require("underscore");
+//Bu dizideki çift sayıları bulan library fonksiyonu pratik basit kolaydır.
+var even = und.filter([1, 2, 3, 4, 5, 6], function (num) { return num % 2 == 0 });
+console.log(even);
+
+var listOfPlays = [{ author: "Tarik", year: 2017, age: 30 },
+{ author: "Ali", year: 2017, age: 41 },
+{ author: "Mehmet", year: 1999, age: 50 },];
+var yazarlar = und.where(listOfPlays, { author: "Mehmet", year: 1999 });
+console.dir(yazarlar);
